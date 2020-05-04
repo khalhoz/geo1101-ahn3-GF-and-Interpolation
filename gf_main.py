b816a4c1a4061a087dd2d4411b22da64918313ba
kriskenesei <kristof.kenesei@outlook.com>
2020-05-04 19:22:58 +0200

    Corrected small mistakes.

--- gf_main.py
+++ gf_main.py
@@ -1,13 +1,13 @@
 ### MAIN FILE FOR PDAL TESTING ###
 
-from processing import initialise, start_pool
+from gf_processing import initialise, start_pool
 
 # You need to specify the folder in which you LAS files are.
 # The folder needs to contain the config JSON in the right format (see example file), with name "config.json".
 # The folder also needs to contain a file called "fnames.txt" with the names of the files you want to process,
 # as also shown in the example file.
 # Use an ABSOLUTE file path to avoid problems (especially when working from virtual environments)!
-target_folder = "C:/Users/geo-geek/example_folder"
+target_folder = "C:/Users/geo-geek/example_folder/"
 # The output files will be written to the target folder too. They will be tagged with "_out".
 
 def main():
